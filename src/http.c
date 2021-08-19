7d528a5b88e1341d7b674f5d8b83cc3709e41805
Lucas.Cruz <lucascruzlcs@gmail.com>
2021-08-19 13:02:28 -0300

    GET request first steps

--- src/http.c
+++ src/http.c
@@ -20,43 +20,67 @@ int main(int argc, char const *argv[]) {
     char * url = "web.ist.utl.pt";///luis.tarrataca/hello.html";
 
     struct sockaddr_in server_add; /*structure for handling internet addresses*/
+    struct sockaddr * destinationSocket;
     struct addrinfo hints;
-    struct addrinfo *result, *rp;
-    int connection_socket;
+    struct addrinfo * result;
 
     memset( &hints, 0, sizeof( hints ));
     hints.ai_family = AF_INET; // 2
   	hints.ai_protocol = SOCK_STREAM; // 1
 
+    /*returns one or more addrinfo structures, each of which contains an Internet address*/
     int success = getaddrinfo( url, NULL, &hints, &result );
 
-    if( !!success ){ //getaddrinfo returns 0 if well succeeded
+    if( success != 0 ){ //getaddrinfo returns 0 if well succeeded
       perror("Error in getaddrinfo");
       exit(EXIT_FAILURE);
     }
 
-    for (rp = result; rp != NULL; rp = rp->ai_next) {
+    char hostname[NI_MAXHOST];
 
-        connection_socket = socket(rp->ai_family, rp->ai_protocol, 0);
+    success = getnameinfo( result->ai_addr, result->ai_addrlen, hostname, NI_MAXHOST, NULL, 0, 0 );
 
-        if (connection_socket == -1){
-            //puts("Viiishi!");
-            perror("Socket error");
-            continue;
-        }
-        ((struct sockaddr_in*) rp->ai_addr)->sin_port = htons(80);
-        int connectionResult = connect(connection_socket, rp->ai_addr, sizeof( struct sockaddr_in ));
+    if( success != 0 ){
+			perror("Error in getnameinfo");
+			return 0;
+		}
 
-        if ( connectionResult == -1){
-            //puts("Não foi dessa vez!");
-            perror("Connection error");
-            continue;
-        }
-        //puts("Tá indo!!!! Amém, Jesus!");
+    printf("IP Address: %d\n", htonl(hostname));
 
-        close(connection_socket);
+    destinationSocket = result->ai_addr;
 
-     }
+    int connection_socket = socket(result->ai_family, result->ai_protocol, 0);
+
+    if (connection_socket == -1){
+        //puts("Viiishi!");
+        perror("Socket error");
+        exit(EXIT_FAILURE);
+    }
+    ((struct sockaddr_in*) result->ai_addr)->sin_port = htons( 80 );
+    int connectionResult = connect(connection_socket, destinationSocket, sizeof( struct sockaddr_in ));
+
+    if ( connectionResult == -1){
+        //puts("Não foi dessa vez!");
+        perror("Connection error");
+        exit(EXIT_FAILURE);
+    }else{
+        puts("Site connection accepted!");
+    }
+
+    char * request  = "GET http://web.ist.utl.pt/luis.tarrataca/hello.html HTTP/1.0\r\nAccept: text/plain, text/html, text/*\r\n\r\n";
+
+  	int number_of_bytes = strlen( request ) * sizeof( char );
+
+  	printf("[[ sendHTMLHeaderRequest ]]\n");
+  	printf("\tSending HTML request message with %d bytes\n", number_of_bytes );
+  	send( connection_socket, request, number_of_bytes, 0 );
+
+		printf("\t-----HTML Request Header-----\n");
+		printf("%s", request );
+
+
+
+    close(connection_socket);
 
     return 0;
 
